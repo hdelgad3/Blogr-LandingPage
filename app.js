bab49ef7e475b922733996e86727db800e90ca42
hdelgad3 <hdelgado9@outlook.com>
2021-08-26 20:37:47 -0600

    style changes

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ toggleBtn.addEventListener("click", () => {
 for (let i = 0; i < headerBtns.length; i++) {
   headerBtns[i].addEventListener("click", () => {
     headerBtns[i].classList.toggle("MenuTabPanel");
+    headerBtns[i].style.content = "/images/icon-arrow-light.svg";
+    let menuTabs = headerBtns[i].nextElementSibling;
+    if (menuTabs.style.display === "none") {
+      console.log("display is none");
+      menuTabs.style.display = "block";
+    } else {
+      menuTabs.style.display = "none";
+      console.log("display is not none");
+    }
   });
 }
