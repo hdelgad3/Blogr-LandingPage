2eb42f02c67907df2c36fccc93fb98a28ff25fce
hdelgad3 <hdelgado9@outlook.com>
2021-09-07 14:40:46 -0600

    finished header look:

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const headerMenu = document.querySelector(".headerMenu");
 const menuTab = document.querySelectorAll(".menuTab");
 
 // Toggle Menu when hamburger button clicked.
-toggleBtn.addEventListener('click', ()=>{
- headerMenu.classList.toggle('displayMenu');
-})
+toggleBtn.addEventListener("click", () => {
+  headerMenu.classList.toggle("displayMenu");
+});
 
 // toggle links in tab
-menuTab.forEach(tab=>{
-  const btn = tab.querySelector('.menuBtn');
-  btn.addEventListener('click', ()=>{
-    menuTab.forEach(item=>{
-      if(item != btn){
-        item.classList.remove('displayLinks');
+menuTab.forEach((tab) => {
+  const btn = tab.querySelector(".menuBtn");
+  // console.log(tab.children[1]);
+  btn.addEventListener("click", () => {
+    menuTab.forEach((item) => {
+      if (item != btn) {
+        item.classList.remove("displayLinks");
       }
     });
-    tab.classList.toggle('displayLinks');
+    tab.classList.toggle("displayLinks");
   });
 });
