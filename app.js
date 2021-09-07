3915199fa125e8352ca307348bfdb799fee12041
Hector <hdelgado9@outlook.com>
2021-09-06 22:01:24 -0600

    donda

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const toggleBtn = document.querySelector(".toggleBtn");
 const headerMenu = document.querySelector(".headerMenu");
 const menuTab = document.querySelectorAll(".menuTab");
 
+// Toggle Menu when hamburger button clicked.
 toggleBtn.addEventListener('click', ()=>{
  headerMenu.classList.toggle('displayMenu');
 })
+
+// toggle links in tab
+menuTab.forEach(tab=>{
+  const btn = tab.querySelector('.menuBtn');
+  btn.addEventListener('click', ()=>{
+    menuTab.forEach(item=>{
+      if(item != btn){
+        item.classList.remove('displayLinks');
+      }
+    });
+    tab.classList.toggle('displayLinks');
+  });
+});
