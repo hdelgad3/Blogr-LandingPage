c6c2a20d069c88674672ce57dbeb0690b9bed8f4
Hector <hdelgado9@outlook.com>
2021-09-06 20:07:54 -0600

    Restarted the look

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const toggleBtn = document.querySelector(".toggleBtn");
-const headerMenu = document.querySelector(".headerLinks");
-const headerBtns = document.querySelectorAll(".headerBtns");
+const headerMenu = document.querySelector(".headerMenu");
+const menuTab = document.querySelectorAll(".menuTab");
 
-toggleBtn.addEventListener("click", () => {
-  headerMenu.classList.toggle("showMenu");
-});
-//idk lol
-for (let i = 0; i < headerBtns.length; i++) {
-  headerBtns[i].addEventListener("click", () => {
-    headerBtns[i].classList.toggle("MenuTabPanel");
-    headerBtns[i].style.content = "/images/icon-arrow-light.svg";
-    let menuTabs = headerBtns[i].nextElementSibling;
-    if (menuTabs.style.display === "none") {
-      console.log("display is none");
-      menuTabs.style.display = "block";
-    } else {
-      menuTabs.style.display = "none";
-      console.log("display is not none");
-    }
-  });
-}
+toggleBtn.addEventListener('click', ()=>{
+ headerMenu.classList.toggle('displayMenu');
+})
